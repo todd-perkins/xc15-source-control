acd25c289a1249ffed4ffc6b2a513b30d1ed8648
Todd Perkins <todd@toddperkins.com>
2024-01-22 12:52:14 -0700

    More fun

--- xc15-source-control/ContentView.swift
+++ xc15-source-control/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Todd!")
+            Text("Hello, Todd!!")
         }
         .padding()
     }

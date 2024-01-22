6a39bd492ed0dbd47fb2887a01c3d8320f30876f
Todd Perkins <todd@toddperkins.com>
2024-01-22 12:12:20 -0700

    Initial Commit

---
+++ xc15-source-control/xc15_source_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  xc15_source_controlApp.swift
+//  xc15-source-control
+//
+//  Created by Todd Perkins on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct xc15_source_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
